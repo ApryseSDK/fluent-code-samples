098db618115e67845a477fde7e781c21855e5ea6
Matt Binsfeld <mbinsfeld@apryse.com>
2025-08-06 13:25:06 -0600

    add a line of text to make it clear we're done generating

--- Embedded .NET Engine Samples/Advanced/DataModeSample/DataModeSample/DataModeSample.cs
+++ Embedded .NET Engine Samples/Advanced/DataModeSample/DataModeSample/DataModeSample.cs
@@ -69,6 +69,7 @@ namespace DataModeSample
             // All finished!
             Console.WriteLine("Finished generating report: " + output.Name);
             Console.WriteLine("Finished generating data file: " + output.Name);
+            Console.WriteLine("Press Any Key To Exit...");
             Console.ReadKey();
         }
     }
